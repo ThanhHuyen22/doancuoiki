8843cd11bcfe9f3fe5ccc1038bde7101d47ae2b2
ADMIN <ADMIN@PC348>
2025-09-09 13:50:24 +0700

    dangnhap

--- src/main/java/dao/DatabaseConnection.java
+++ src/main/java/dao/DatabaseConnection.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package dao;
 
 import java.sql.Connection;
@@ -9,26 +5,39 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-
-    // Sửa lại URL/USER/PASSWORD theo môi trường của bạn
-    private static final String URL
-            = "jdbc:sqlserver://DESKTOP-L1OKGPV\\SQLEXPRESS2022:1433;"
-        + "databaseName=QuanLyTiemGiatUi;"
-        + "encrypt=true;trustServerCertificate=true;";
+    // URL kết nối SQL Server
+    private static final String URL = "jdbc:sqlserver://DESKTOP-GDJGQDK\\SQLEXPRESS:1433;"
+                                    + "databaseName=QuanLyTiemGiatUi;"
+                                    + "encrypt=true;trustServerCertificate=true;";
+    
+    // Tài khoản SQL Server
     private static final String USER = "sa";
     private static final String PASSWORD = "sa";
 
+    // Nạp driver
     static {
         try {
-            // SQL Server JDBC driver
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         } catch (ClassNotFoundException e) {
-            // Nếu thiếu driver -> add mssql-jdbc vào project libraries
-            throw new RuntimeException("Không tìm thấy SQLServer JDBC Driver", e);
+            throw new RuntimeException("Không tìm thấy SQL Server JDBC Driver", e);
         }
     }
 
+    // Hàm trả về Connection
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    // Test thử kết nối
+    public static void main(String[] args) {
+        try (Connection conn = getConnection()) {
+            if (conn != null) {
+                System.out.println(" Kết nối SQL Server thành công!");
+            } else {
+                System.out.println(" Kết nối thất bại!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
